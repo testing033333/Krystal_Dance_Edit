9c7e463f93b475c9d4e6dad380ae079b5135a8c5
totalpessimist333 <nezlaasoloter@gmail.com>
2025-07-02 16:48:40 +0800

    Update OldReanimation.lua

--- OldReanimation.lua
+++ OldReanimation.lua
@@ -1787,7 +1787,7 @@
 			char.Humanoid.WalkSpeed = 6*char:GetScale()
 			timeposcur = sound69.TimePosition 
 	sound69:Play()
-			playanim(86485871533985,0.15)
+			playanim(86485871533985,0.25)
 		else
 			stopanim()
 		end
