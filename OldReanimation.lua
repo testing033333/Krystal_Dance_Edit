be475fd2d446e0b301d24007c1862459d5a9eab8
totalpessimist333 <nezlaasoloter@gmail.com>
2025-07-03 20:10:55 +0800

    Update OldReanimation.lua

--- OldReanimation.lua
+++ OldReanimation.lua
@@ -1034,9 +1034,9 @@
 				sound69.PlaybackSpeed = 1
 				if playbacktrack == true then 
 							if lol ~= true then 
-				sound69.SoundId = customasset("Dances/NO MERCY III [FULL ALBUM].mp3")
+				sound69.SoundId = customasset("Dances/Amalgam But Breakcore (Filler).mp3")
 					else 
-						sound69.SoundId = customasset("Dances/NO MERCY III [FULL ALBUM].mp3")  
+						sound69.SoundId = customasset("Dances/Amalgam But Breakcore (Filler).mp3")  
 					end
 				sound69.Volume = .75
 				else 
