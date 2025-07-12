cbd842c56bb28a1ef2d7d21c7514fafda6223939
totalpessimist333 <nezlaasoloter@gmail.com>
2025-07-13 07:19:10 +0800

    Update JustABaseplateOnly.lua

--- JustABaseplateOnly.lua
+++ JustABaseplateOnly.lua
@@ -1399,7 +1399,7 @@ end
 			stopanim()
 			dancing = true
 			task.wait(.005)
-			sound69.SoundId = customasset("Dances/why as soon as we become so close we have to say goodbye.mp3")
+			sound69.SoundId = customasset("Dances/goodbye.mp3")
 			timeposcur = sound69.TimePosition 
 	sound69:Play()
 			playanim(92187683724153)
