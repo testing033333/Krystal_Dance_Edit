c34e33f1d048bbe8d05647cd64f2e3f131f8a32f
totalpessimist333 <nezlaasoloter@gmail.com>
2025-07-19 20:44:21 +0800

    Update JustABaseplateOnly.lua

--- JustABaseplateOnly.lua
+++ JustABaseplateOnly.lua
@@ -703,7 +703,7 @@ end
 				sound69.SoundId = customasset("Dances/asdf.mp3")
 				timeposcur = sound69.TimePosition 
 	sound69:Play()
-				playanim(82123030025988)
+				playanim(98605693116996)
 			else
 				stopanim()
 			end
