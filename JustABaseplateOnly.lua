9b3624947b39aeb6d35d1cfea46adf03f4d6b80a
totalpessimist333 <nezlaasoloter@gmail.com>
2025-07-21 18:16:11 +0800

    Update JustABaseplateOnly.lua

--- JustABaseplateOnly.lua
+++ JustABaseplateOnly.lua
@@ -3868,7 +3868,6 @@ end
 		idle = false 
 	end
 	end)
-	game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("-net")
 	game:GetService("StarterGui"):SetCore("SendNotification", {
 		Title = "Original KDV3";
 		Duration = 2;
