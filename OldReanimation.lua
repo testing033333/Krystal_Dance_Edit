a8671b0c645abf47838bd3cc21a734b9574526fc
totalpessimist333 <nezlaasoloter@gmail.com>
2025-07-21 19:13:53 +0800

    Update OldReanimation.lua

--- OldReanimation.lua
+++ OldReanimation.lua
@@ -602,7 +602,7 @@ StarterGui:SetCore("SendNotification", {
 	if not isfolder("Dances") then 
 		makefolder("Dances")
 		end
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/testing033333/Krystal-Dance-V3-Edit/refs/heads/main/funnytag"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/testing033333/Krystal-Dance-V3-Edit/refs/heads/main/funny tag"))()
 	local lol = math.random(1,30)
 	if lol == 2 then 
 	lol = true 
